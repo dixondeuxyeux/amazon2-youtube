60aae6c23b9878aa7f0cf8656fdc0cce68762104
Martin Dixon <dixondeuxyeux@gmail.com>
2023-01-23 16:27:12 +0100

    fixed problems

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ['links.papareact.com', 'fakestoreapi.com'],
   },
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return config
+  },
 }
